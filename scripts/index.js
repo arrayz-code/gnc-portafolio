b618517901f8b46fd643e853e6752a6be875a816
anedarlo <arraizangel5@gmail.com>
2023-05-11 15:51:48 -0400

    Primer commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const miTitulo = document.querySelector("h1");
+miTitulo.textContent = "Global Network Connection";
+
+const toggleButton = document.getElementById('button-menu')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+})
